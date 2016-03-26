681b37e2749202c0697c680faef1cd169d5f3f04
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-03-26 11:34:48 +0200

    add tests

--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @problem_one = problems(:one)
+    @problem_two = problems(:two)
+  end
+
+  test "should be solved" do
+    assert_equal true, @problem_one.solved?
+  end
+
+  test "should not be solved" do
+    assert_not @problem_two.solved?
+  end
 end
--- test/models/solution_test.rb
+++ test/models/solution_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class SolutionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @solution = solutions(:test_solution)
+    @wrong_solution = solutions(:test_solution_wrong)
+  end
+
+  test "should be valid" do
+    assert @solution.valid?
+  end
+
+  test "should not be valid without present content" do
+    @solution.content = ' '
+    assert_not @solution.valid?
+  end
+
+  test "should not be valid without present test_status" do
+    @solution.test_status = ' '
+    assert_not @solution.valid?
+  end
+
+  test "should be correct" do
+    assert_equal true, @solution.correct?
+  end
+
+  test "should not be correct" do
+    assert_not @wrong_solution.correct?
+  end
 end
