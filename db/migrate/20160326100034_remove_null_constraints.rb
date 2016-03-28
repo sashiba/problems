d7f2ca8e74d9568f2c771ea73b2981a9c4830722
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-03-28 20:09:28 +0300

    fix tests

---
+++ db/migrate/20160326100034_remove_null_constraints.rb
@@ -0,0 +1,7 @@
+class RemoveNullConstraints < ActiveRecord::Migration[5.0]
+  def change
+    change_column_null :solutions, :test_output, from: false, to: true
+    change_column_null :solutions, :test_status, from: false, to: true
+    change_column_null :solutions, :checked_at, from: false, to: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326100032) do
+ActiveRecord::Schema.define(version: 20160326100034) do
 
   create_table "problems", force: :cascade do |t|
     t.integer  "user_id",    null: false
